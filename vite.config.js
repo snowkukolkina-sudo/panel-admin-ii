a263d31cdcbf60d1780227edb5f0e4207e05f217
snowkukolkina-sudo <snowkukolkina@gmail.com>
2025-09-20 04:51:30 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/panel-admin-ii/',
+  plugins: [react()],
 });
